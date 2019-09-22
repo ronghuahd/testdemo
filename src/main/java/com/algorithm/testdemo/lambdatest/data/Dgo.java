5f2aba812cf84434af39f67700f4b59250dcdbf2
it <jitianyou123@163.com>
2019-09-22 23:16:45 +0800

    change one

---
+++ src/main/java/com/algorithm/testdemo/lambdatest/data/Dgo.java
@@ -0,0 +1,13 @@
+package com.algorithm.testdemo.lambdatest.data;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @Author: ronghua
+ * @Date: 2019/9/22
+ */
+public class Dgo {
+    private void getTest(){
+        System.out.println("get");
+    }
+}
